08b9c090597aecb335ca504dd60d18b41104034e
Daniel <adeboyed93@gmail.com>
2022-08-04 19:49:44 +0300

    new

--- obbkit.rb
+++ obbkit.rb
@@ -1,17 +1,81 @@
-
 class Obbkit < Formula
-    desc "A cli tool for generating boilerplates with onboardbase configured  "
-    homepage ""
-    url "https://github.com/AdeboyeDN/obbkit/archive/refs/tags/v0.0.1.tar.gz"
-    sha256 "b09a559a69af6a694b26c5884cee8ac9c32a00e9cdc2fdbb9b5489c060a62b0d"
-    license "MIT"
-    
-    def install
+  desc "A cli tool for generating boilerplates with onboardbase configured  "
+  homepage ""
+  url "https://github.com/AdeboyeDN/obbkit/archive/refs/tags/v0.0.1.tar.gz"
+  sha256 "b09a559a69af6a694b26c5884cee8ac9c32a00e9cdc2fdbb9b5489c060a62b0d"
+  license "MIT"
 
-      system "./configure", *std_configure_args, "--disable-silent-rules"
-    end
-  
-    test do
-      system "false"
-    end
+  def install
+    system "./configure", *std_configure_args, "--disable-silent-rules"
   end
+
+  test do
+    system "false"
+  end
+end
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+"/opt/homebrew/Library/Taps/homebrew/homebrew-core/Formula/obbkit.rb" 33L, 1444B
+class Obbkit < Formula
+  desc "A cli tool for generating boilerplates with onboardbase configured  "
+  homepage ""
+  url "https://github.com/AdeboyeDN/obbkit/archive/refs/tags/v0.0.1.tar.gz"
+  sha256 "b09a559a69af6a694b26c5884cee8ac9c32a00e9cdc2fdbb9b5489c060a62b0d"
+  license "MIT"
+
+
+  def install
+    system "./configure", *std_configure_args, "--disable-silent-rules"
+  end
+
+  test do
+    system "false"
+  end
+end
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+"/opt/homebrew/Library/Taps/homebrew/homebrew-core/Formula/obbkit.rb" 33L, 1444B
